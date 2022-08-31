aa219237eddcd3b0ef1e05cf566719ef144789f0
helloalicesmith <helloalicesmith@gmail.com>
2022-08-31 14:53:36 +0300

    linter

--- blog/views.py
+++ blog/views.py
@@ -6,16 +6,19 @@ from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {
         'posts': posts,
     })
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -32,6 +35,7 @@ def post_new(request):
         form = PostForm()
 
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
